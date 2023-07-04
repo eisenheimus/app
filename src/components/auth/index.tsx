ab98ada5908bfd79d6b78e7d253132229c50cf66
Антон Снигир <6283064@gmail.com>
2023-07-05 00:50:07 +0700

    login form

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -2,12 +2,26 @@ import * as React from 'react';
 import { useLocation } from 'react-router-dom';
 import Login from './login';
 import Register from './register';
+import { Box } from '@mui/material';
+import './index.css';
 
 const AuthRootComponent = () => {
-    const location = useLocation()
-    return (
-        location.pathname === '/login' ? <Login /> : (location.pathname === '/register' ? <Register /> : <h1> qwe </h1> )
-    )
-}
+	const location = useLocation();
+	return (
+		<div className="root">
+			<div className="form">
+				<Box>
+					{location.pathname === '/login' ? (
+						<Login />
+					) : location.pathname === '/register' ? (
+						<Register />
+					) : (
+						<h1> qwe </h1>
+					)}
+				</Box>
+			</div>
+		</div>
+	);
+};
 
 export default AuthRootComponent;
--- src/components/auth/login/index.tsx
+++ src/components/auth/login/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { TextField, Button, Typography } from '@mui/material';
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login page</h1>
-        </div>
-    );
-}
+	return (
+		<>
+			<Typography variant="h3" fontFamily="Popins" textAlign="center">
+				Авторизация
+			</Typography>
+			<Typography variant="body1" textAlign="center">
+				Введите ваш логин и пароль
+			</Typography>
+			;
+			<TextField
+				fullWidth={true}
+				margin="normal"
+				id="outlined-basic"
+				label="Outlined"
+				variant="outlined"
+			/>
+			<TextField
+				fullWidth={true}
+				margin="normal"
+				id="outlined-basic"
+				label="Outlined"
+				variant="outlined"
+			/>
+			<Button variant="contained" sx={{ width: '50%', display: 'flex', alignSelf: 'center' }}>
+				Войти
+			</Button>
+		</>
+	);
+};
 
 export default Login;
